bca6f899fca2ed526669053967afad7733d7aefb
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-11-10 10:08:55 -0500

    Create Storage.sol

---
+++ Storage.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+    uint storedData;
+    function set(uint x) public {
+        storedData = x;
+    }
+    function get() constant public returns (uint){
+        return storedData;
+    }
+    function increment (uint n) public {
+        storedData = storedData + n;
+    }
+    function decrement (uint n) public {
+        storedData = storedData - n;
+    }
+}
